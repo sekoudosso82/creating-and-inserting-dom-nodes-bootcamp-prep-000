fa0bfa2e7c87bf9c4a60e74455d04a219ef384d6
Sekou Dosso <sekou.dosso82@gmail.com>
2019-12-31 17:40:54 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,2 +1,37 @@
-//this file is optional
-//you can use this to paste in js code and have it run when index.html is loaded
+document.createElement()
+
+var element = document.createElement('div');
+
+element.innerHTML = 'Hello, DOM!';
+element.style.backgroundColor = '#f9f9f9';
+
+appendChild()
+
+document.body.appendChild(element);
+
+element.style.textAlign = 'center';
+
+// We can append elements to that element://
+
+var ul = document.createElement('ul');
+ 
+for (let i = 0; i < 3; i++) {
+  let li = document.createElement('li');
+  li.innerHTML = (i + 1).toString();
+  ul.appendChild(li);
+}
+ 
+element.appendChild(ul);
+
+ul.style.textAlign = 'left';
+
+removeChild()
+
+// Now let's remove one of those lis.
+
+ul.removeChild(ul.querySelector('li:nth-child(2)'));
+
+element.remove()
+// We can just call remove() on the element itself:// 
+
+ul.remove();
